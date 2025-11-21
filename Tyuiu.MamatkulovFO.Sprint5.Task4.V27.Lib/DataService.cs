626859722aea4f05a78314bca2ded6bdd972732f
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-11-21 20:48:38 +0500

    Task4

--- Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Lib/DataService.cs
+++ Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Lib/DataService.cs
@@ -6,14 +6,18 @@ namespace Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            string data = File.ReadAllText(path);
-            double x = double.Parse(data);
+            if (!File.Exists(path))
+                throw new FileNotFoundException("Fayl topilmadi", path);
+
+            string data = File.ReadAllText(path).Trim();
+            if (!double.TryParse(data, out double x))
+                throw new FormatException("Fayl ichida son emas");
 
             double numerator = Math.Pow(x, 3) - 4 * x;
             double denominator = Math.Cos(x);
 
             if (Math.Abs(denominator) < 1e-10)
-                throw new DivideByZeroException("cos(x) = 0");
+                throw new DivideByZeroException("cos(x) = 0, hisoblash mumkin emas");
 
             double result = numerator / denominator;
             return Math.Round(result, 3);
