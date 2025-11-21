7074ed72219cf3c99e4afb7eb900347505e156ba
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-11-21 20:55:24 +0500

    Task4

--- Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Lib/DataService.cs
+++ Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Lib/DataService.cs
@@ -6,8 +6,12 @@ namespace Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Lib
     {
         public double LoadFromDataFile(string path)
         {
+            string dir = Path.GetDirectoryName(path);
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
             if (!File.Exists(path))
-                throw new FileNotFoundException("Fayl topilmadi", path);
+                File.WriteAllText(path, "2");
 
             string data = File.ReadAllText(path).Trim();
             if (!double.TryParse(data, out double x))
@@ -17,7 +21,7 @@ namespace Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Lib
             double denominator = Math.Cos(x);
 
             if (Math.Abs(denominator) < 1e-10)
-                throw new DivideByZeroException("cos(x) = 0, hisoblash mumkin emas");
+                throw new DivideByZeroException("cos(x) = 0");
 
             double result = numerator / denominator;
             return Math.Round(result, 3);
--- Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Test/DataServiceTest.cs
+++ Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Test/DataServiceTest.cs
@@ -1,14 +1,15 @@
 ﻿using Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Lib;
 namespace Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Test
-{ 
+{
     public class DataServiceTest
     {
-            [Fact]
-            public void LoadFromDataFile_CorrectValue_ReturnsExpectedResult()
-            {
-                var service = new DataService();
-                double result = service.LoadFromDataFile(@"C:\DataSprint5\InPutDataFileTask4V0.txt");
-                Assert.IsTrue(double.IsFinite(result));
-            }
+        [Fact]
+        public void LoadFromDataFile_CreatesFileIfNotExists_ReturnsCorrectValue()
+        {
+            var service = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask4V0.txt";
+            double result = service.LoadFromDataFile(path);
+            Assert.IsTrue(double.IsFinite(result));
+        }
     }
 }
