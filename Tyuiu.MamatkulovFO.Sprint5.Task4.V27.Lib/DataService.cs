0f04ec4883c8a95b8998a2f0b749636fcda64cd5
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-11-26 19:20:27 +0500

    Task4

--- Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Lib/DataService.cs
+++ Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Lib/DataService.cs
@@ -7,20 +7,15 @@ namespace Tyuiu.MamatkulovFO.Sprint5.Task4.V27.Lib
     {
         public double CalculateFormulaFromDataFile(string tempPath)
         {
-            throw new NotImplementedException();
-        }
-
-        public double LoadFromDataFile(string path)
-        {
           
-            string content = File.ReadAllText(path).Trim();
+            string content = File.ReadAllText(tempPath).Trim();
             double x = double.Parse(content);
 
-           
+        
             double numerator = Math.Pow(x, 3) - 4 * x;
             double denominator = Math.Cos(x);
 
-           
+            
             if (Math.Abs(denominator) < 1e-10)
                 throw new DivideByZeroException("Cos(x) nolga teng!");
 
--- Tyuiu.MamatkulovFO.Sprint5.Task4.V27/Program.cs
+++ Tyuiu.MamatkulovFO.Sprint5.Task4.V27/Program.cs
@@ -9,21 +9,16 @@ namespace Tyuiu.MamatkulovFO.Sprint5.Task4.V27
             Console.WriteLine("Формула: y = (x^3 - 4x) / cos(x)");
             Console.WriteLine("--------------------------------------------------");
 
-            
             string filePath = @"C:\DataSprint5\InPutDataFileTask4V27.txt";
-
-          
             Directory.CreateDirectory(@"C:\DataSprint5");
 
-           
             if (!File.Exists(filePath))
             {
                 File.WriteAllText(filePath, "-1.26");
             }
 
             var service = new DataService();
-            double result = service.CalculateFormulaFromDataFile(filePath);
-
+            double result = service.CalculateFormulaFromDataFile(filePath); 
             Console.WriteLine($"Результат: {result:F3}");
             Console.WriteLine("--------------------------------------------------");
             Console.WriteLine("Нажмите Enter для выхода...");
