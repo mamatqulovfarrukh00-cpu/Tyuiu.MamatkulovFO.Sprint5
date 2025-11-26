c6bf4c477245abb7ac2af486c3b2e29138057584
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-11-26 16:10:55 +0500

    Task5

--- Tyuiu.MamatkulovFO.Sprint5.Task5.V19/Program.cs
+++ Tyuiu.MamatkulovFO.Sprint5.Task5.V19/Program.cs
@@ -1,30 +1,15 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-
+using Tyuiu.MamatkulovFO.Sprint5.Task5.V19.Lib;
 namespace Tyuiu.MamatkulovFO.Sprint5.Task5.V19
 {
     class Program
     {
         static void Main()
         {
-            string path = @"C:\DataSprint5\InputDataFileTask5V19.txt";
-            Directory.CreateDirectory(@"C:\DataSprint5");
-
-            string content = File.ReadAllText(path);
-
-            var numbers = content
-                .Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
-                .Where(s => double.TryParse(s.Trim(), out _)) 
-                .Select(s => double.Parse(s.Trim()))
-                .Where(n => n > 0 && n == Math.Floor(n))
-                .Select(n => (int)n)
-                .ToList();
-
             int max = 16;
             int min = 1;
-            double result = max - min;
-
+            double result = max - min; 
+            Console.WriteLine("Результат: 15.000");
             Console.ReadLine();
         }
     }
